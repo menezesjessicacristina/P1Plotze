ac035ec63f10b99c7d73d983fa7a01125d4b0e6c
menezesjessicacristina <menezesjessica.cristina@gmail.com>
2024-03-27 21:22:59 -0300

    efetuadas modificações

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:meu_login/views/cadastro_view.dart';
+import 'package:meu_login/views/redefinir_senha_view.dart';
 
 void main() {
   runApp(
@@ -20,7 +22,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Meu App',
-      home: PrincipalView(),
+      home: RedefinirSenhaView(),
     );
   }
 }
@@ -49,120 +51,135 @@ class _PrincipalViewState extends State<PrincipalView> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color.fromARGB(255, 253, 238, 99),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
-          child: Form(
-            key: formKey,
-            child: Column(children: [
-              //Adicionar a imagem
-              Image.asset(
-                'lib/imagem/carrinho3.gif',
-                width: 200,
-                height: 200,
-              ),
-              TextFormField(
-                controller: txtValor1,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+            child: Form(
+              key: formKey,
+              child: Column(children: [
+                //Adicionar a imagem
+                Image.asset(
+                  'lib/imagem/carrinho3.gif',
+                  width: 200,
+                  height: 200,
+                ),
+                TextFormField(
+                  controller: txtValor1,
 
-                style: TextStyle(fontSize: 32),
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.email_outlined),
-                ),
-                //
-                // VALIDAÇÃO
-                //
-                validator: (value) {
-                  if (value == null) {
-                    return 'Campo Obrigatório';
-                  } else if (value.isEmpty) {
-                    return 'Campo Obrigatório';
-                  }
-                  if (value.contains('@')) {
-                    return null;
-                  } else {
-                    return 'Informe um email valido';
-                  }
-                },
-              ),
-              // CAMPO DE TEXTO
-              //
-              SizedBox(height: 30),
-              TextFormField(
-                obscureText: true,
-                controller: txtValor2,
-
-                style: TextStyle(fontSize: 32),
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.password_sharp),
-                ),
-                //
-                // VALIDAÇÃO
-                //
-                validator: (value) {
-                  if (value == null) {
-                    return 'Informe um valor';
-                  } else if (value.isEmpty) {
-                    return 'Informe um valor';
-                  } else if (double.tryParse(value) == null) {
-                    return 'Informe um valor Numerico';
-                    //Retornar null significa sucesso na validação
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 30),
-              //
-              // BOTÃO
-              //
-              //ElevatedButton, OutlinedButton, TextButton
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 141, 181, 226),
-                  foregroundColor: Colors.blue.shade900,
-                  minimumSize: Size(200, 50),
-                  shadowColor: Color.fromARGB(255, 3, 17, 43),
-                ),
-                onPressed: () {},
-                child: Text('Entrar'),
-              ),
-              SizedBox(height: 20),
-
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Esqueci minha senha',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Cadastre-se',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-              ),
-
-              SizedBox(height: 120),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Sobre Nós',
-                      style: TextStyle(color: Colors.black),
-                    ),
+                  style: TextStyle(fontSize: 18),
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email_outlined),
                   ),
-                ],
-              )
-            ]),
+                  //
+                  // VALIDAÇÃO
+                  //
+                  validator: (value) {
+                    if (value == null) {
+                      return 'Campo Obrigatório';
+                    } else if (value.isEmpty) {
+                      return 'Campo Obrigatório';
+                    }
+                    if (value.contains('@')) {
+                      return null;
+                    } else {
+                      return 'Informe um email valido';
+                    }
+                  },
+                ),
+                // CAMPO DE TEXTO
+                //
+                SizedBox(height: 30),
+                TextFormField(
+                  obscureText: true,
+                  controller: txtValor2,
+
+                  style: TextStyle(fontSize: 18),
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.password_sharp),
+                  ),
+                  //
+                  // VALIDAÇÃO
+                  //
+                  validator: (value) {
+                    if (value == null) {
+                      return 'Informe um valor';
+                    } else if (value.isEmpty) {
+                      return 'Informe um valor';
+                    } else if (double.tryParse(value) == null) {
+                      return 'Informe um valor Numerico';
+                      //Retornar null significa sucesso na validação
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 30),
+                //
+                // BOTÃO
+                //
+                //ElevatedButton, OutlinedButton, TextButton
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 141, 181, 226),
+                    foregroundColor: Colors.blue.shade900,
+                    minimumSize: Size(200, 50),
+                    shadowColor: Color.fromARGB(255, 3, 17, 43),
+                  ),
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {}
+                  },
+                  child: Text('Entrar'),
+                ),
+                SizedBox(height: 20),
+
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => RedefinirSenhaView()),
+                    );
+                  },
+                  child: Text(
+                    'Esqueci minha senha',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => CadastroView()),
+                    );
+                  },
+                  child: Text(
+                    'Cadastre-se',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                ),
+
+                SizedBox(height: 80),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Sobre',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                )
+              ]),
+            ),
           ),
         ),
       ),
---
+++ lib/views/redefinir_senha_view.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:meu_login/main.dart';
+
+class RedefinirSenhaView extends StatefulWidget {
+  const RedefinirSenhaView({super.key});
+
+  @override
+  State<RedefinirSenhaView> createState() => _RedefinirSenhaViewState();
+}
+
+class _RedefinirSenhaViewState extends State<RedefinirSenhaView> {
+  // Atributos
+  //Chave identificadora do Form
+  var formKey = GlobalKey<FormState>();
+
+  //Controladores dos Campos de Texto
+  var txtemail = TextEditingController();
+  String msg =
+      'Insira o seu email de usuário e enviaremos um link para você voltar a acessar a sua conta.';
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 253, 238, 99),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+          child: Form(
+            key: formKey,
+            child: Column(children: [
+              const SizedBox(height: 30),
+              Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, border: Border.all(width: 3)),
+                child: const Center(
+                  child: Icon(
+                    Icons.lock_outlined,
+                    size: 90,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 30),
+              Text(
+                'Problemas para entrar',
+                style: TextStyle(color: Colors.blue[900], fontSize: 18),
+              ),
+              Text(
+                msg,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+
+              TextFormField(
+                controller: txtemail,
+
+                style: const TextStyle(fontSize: 18),
+                decoration: const InputDecoration(
+                  labelText: 'E-mail',
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email_outlined),
+                ),
+                //
+                // VALIDAÇÃO
+                //
+                validator: (value) {
+                  if (value == null) {
+                    return 'Campo Obrigatório';
+                  } else if (value.isEmpty) {
+                    return 'Campo Obrigatório';
+                  }
+                  if (value.contains('@')) {
+                    return null;
+                  } else {
+                    return 'Informe um email valido';
+                  }
+                },
+              ),
+
+              const SizedBox(height: 30),
+              //
+              // BOTÃO
+              //
+              //ElevatedButton, OutlinedButton, TextButton
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 141, 181, 226),
+                  foregroundColor: Colors.blue.shade900,
+                  minimumSize: const Size(200, 50),
+                  shadowColor: const Color.fromARGB(255, 3, 17, 43),
+                ),
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {}
+                },
+                child: const Text('Enviar'),
+              ),
+              const SizedBox(height: 30),
+
+              Text(
+                '--------- OU ---------',
+                style: TextStyle(fontSize: 14),
+              ),
+              const SizedBox(height: 30),
+
+              Text('Criar outra conta',
+                  style: TextStyle(fontSize: 18), textAlign: TextAlign.center),
+              const SizedBox(height: 80),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const PrincipalView()),
+                  );
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(Icons.undo, color: Color.fromARGB(255, 5, 62, 128)),
+                    Text(
+                      'Voltar',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 5, 62, 128), fontSize: 18),
+                    ),
+                  ],
+                ),
+              )
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
