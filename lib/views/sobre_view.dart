b8c37e36cee42901ab7ebf16bb0c4b73e66f287d
menezesjessicacristina <menezesjessica.cristina@gmail.com>
2024-03-29 22:22:29 -0300

    Criacao da tela sobre

--- lib/views/sobre_view.dart
+++ lib/views/sobre_view.dart
@@ -14,11 +14,30 @@ class _SobreViewState extends State<SobreView> {
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Sobre'),
+          backgroundColor: Color.fromARGB(255, 253, 238, 99),
         ),
-        body: Column(
-          children: [
-            Image.asset('lib/asset/imagem/gifapp_compras.gif'),
-          ],
+        body: Container(
+          color: Colors.black87,
+          child: Column(children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width / 4,
+                  vertical: 40),
+              child: Image.asset(
+                'lib/imagem/gifapp_compras.gif',
+                fit: BoxFit.fill,
+                width: 200,
+                height: 200,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+              child: Text(
+                  'Lista de Compras: Este aplicativo oferece uma experiência completa, Lembre-se de escolher um aplicativo que atenda às suas necessidades e preferências pessoais. Feliz compras! ',
+                  style: TextStyle(color: Colors.yellow[50]),
+                  textAlign: TextAlign.center),
+            )
+          ]),
         ),
       ),
     );
